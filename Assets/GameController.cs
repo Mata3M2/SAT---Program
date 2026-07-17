aa1ad3a9d390614a188567defcb39bb0d054117b
Mata3M2 <kkna49513@gmail.com>
2026-07-17 17:54:26 +1000

    Unity Console error fixed

--- Assets/GameController.cs
+++ Assets/GameController.cs
@@ -2,12 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class　UNityDayo: MonoBehaviour
+public class GameController : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("Hello, World");
         
     }
 
