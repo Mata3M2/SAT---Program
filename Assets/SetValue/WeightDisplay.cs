177c496a0b4f0a558150d80592b9d0f347276abd
Mata3M2 <kkna49513@gmail.com>
2026-07-25 17:23:18 +1000

    Add heart-mark_red.png.meta file with texture import settings

--- Assets/SetValue/WeightDisplay.cs
+++ Assets/SetValue/WeightDisplay.cs
@@ -8,22 +8,19 @@ public class WeightDisplay : MonoBehaviour
     private float weight = 0.2f;
     private Text messageText;
     public Text weightText;
+    public Text scoreBoard;
     // Start is called before the first frame update
     void Start()
     {
         messageText = GameObject.Find("WeightText").GetComponent<Text>();
         weightText = GameObject.Find("WeightText").GetComponent<Text>();
+        scoreBoard = GameObject.Find("Score_Like").GetComponent<Text>();
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.A))
-        {
-            weight++;
-            Debug.Log("重み: " + weight + "点");
-
-            weightText.text = "(Weight :" + weight + ")";
-        }
+    
     }
 }
--- Assets/SetValue/WeightManager.cs
+++ Assets/SetValue/WeightManager.cs
@@ -8,9 +8,23 @@ public class WeightManager : MonoBehaviour
     public float likeScore; // Variable to store the weight value       
     public WeightDisplay weightDisplay; // Reference to the WeightDisplay script
 
+    public enum WeightType //Enumeration type(enum is a special data type that enables for a variable to be a set of predefined constants)
+{
+    Like, 
+    Repost,
+    Bookmark,
+    Comment
+}
+    private WeightType currentWeightType = WeightType.Like; // Default weight type
+    public void OnNextButton()
+    {
+        currentWeightType = WeightType.Like;
+        
+        }
+
     void Start()
     {
-        
+        likeScore = 0.2f; // Initialize the weight value
     }
 
     // Update is called once per frame
@@ -18,5 +32,10 @@ public class WeightManager : MonoBehaviour
     {
         Debug.Log("現在の重み: " + likeScore);
         weightDisplay.weightText.text = "(Weight :" + likeScore + ")";
+        weightDisplay.scoreBoard.text = "Like: " + likeScore;
+
+         
     }
+
+    
 }
