aad7161a263bab45dea2e3deca1dff2eb6cc731d
Mata3M2 <kkna49513@gmail.com>
2026-07-25 13:33:29 +1000

    Add new scene and script for setting arm values
    - Created a new scene "SetArmValue.unity" and added it to the Editor Build Settings.
    - Implemented a new script "SetArmValue.cs" with basic structure for future functionality.
    - Added corresponding meta files for both the scene and the script.

---
+++ Assets/SetArmValue.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetArmValue : MonoBehaviour
+{
+    public float likeValue;
+    public float repostValue;
+    public float commentValue;
+    public float viewValue;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
