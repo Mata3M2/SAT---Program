8e93e47abea440726a4a67048074f931694b283a
Mata3M2 <kkna49513@gmail.com>
2026-07-27 21:21:57 +1000

    Add config/コンフィグを設定した
    Add weight configuration and update WeightManager for dynamic weight handling
    
    コンフィグを設定することによって、別のシーンに切り替わっても数値を保存できるようにできました。

--- Assets/SetValue/WeightManager.cs
+++ Assets/SetValue/WeightManager.cs
@@ -8,19 +8,27 @@ using UnityEngine.SceneManagement;
 
 public class WeightManager : MonoBehaviour
 {
-     // Variable to store the weight value   
+
+     [SerializeField] private Config weightConfig;  
      public PanelManager panelManager;  
      public WeightSliderController sliderController;
      public ChangeSceneManager changeSceneManager;
     
-
-    public enum WeightType  {Like = 0, Repost=1, Bookmark=2, Comment=3, ToNextPage=4} //Enumeration type(enum is a special data type that enables for a variable to be a set of predefined constants)
+    #region WeightType
+    public enum WeightType  
+    {
+        Like = 0, 
+        Repost=1, 
+        Bookmark=2, 
+        Comment=3, 
+        ToNextPage=4} //Enumeration type(enum is a special data type that enables for a variable to be a set of predefined constants)
+    #endregion
 
     [Header("Current Mode）")]
     public WeightType currentWeightIndex; // Current weight type
     
     [Header("Weight Values")]
-    public float[] weightValues = new float[4] { 0.2f, 0.5f, 0.3f, 0.8f }; // Array to store weight values for each type
+    public float[] weightValues = new float[4] { 0.2f, 0.5f, 0.3f, 0.8f }; // ここで各 WeightType のフロートを扱っている, なので、ボタンで数値を確定したら、この数値が動くという仕組みだ。
 
     
     public WeightDisplay weightDisplay; // Reference to the WeightDisplay script
@@ -43,13 +51,20 @@ public class WeightManager : MonoBehaviour
             sliderController.OnWeightChanged(weight);
         }
     }
+
+#region Button Attributes
     public void OnNextButton()
         {
             float currentSliderValue = sliderController.weight;
-            
+
+            #region WeightSetting of Each WeightTpye
             switch (currentWeightIndex) //ここは重み付けをするときにスコアを設定するプログラムです
             {
                 case WeightType.Like: //数値を確定する
+                //★Config（ScriptableObject）の配列[0]に保存
+                //
+                if (weightConfig != null) weightConfig.weightValues[0] = currentSliderValue;
+
                 weightValues[0] = currentSliderValue; 
                 weight = sliderController.weightSlider.value; //ウェイトスライダーの値をゲットしてる
                 weight = float.Parse(weight.ToString("F1"));
@@ -58,6 +73,8 @@ public class WeightManager : MonoBehaviour
                 break;
 
                 case WeightType.Repost:
+                if (weightConfig != null) weightConfig.weightValues[1] = currentSliderValue;
+
                 weightValues[1] = currentSliderValue;
                 weight = sliderController.weightSlider.value;
                 weight = float.Parse(weight.ToString("F1"));
@@ -65,24 +82,27 @@ public class WeightManager : MonoBehaviour
                 break;
 
                 case WeightType.Bookmark:
+                if (weightConfig != null) weightConfig.weightValues[2] = currentSliderValue;
+
+
                 weight = sliderController.weightSlider.value; 
                 weight = float.Parse(weight.ToString("F1"));
                 score_Bookmark.text = "Bookmark: " + weight;
                 break;
 
                 case WeightType.Comment:
+                if (weightConfig != null) weightConfig.weightValues[3] = currentSliderValue;
+              
                 weightValues[3] = currentSliderValue;
                 weight = sliderController.weightSlider.value; 
                 weight = float.Parse(weight.ToString("F1"));
                 score_Comment.text = "Comment: " + weight;
                 break;
                 
-                
-
             }
-            
+            #endregion
 
-            
+            //ここに[パネルの移動]や[次のシーンへ移動する]処理が入っている。
             int value = (int)currentWeightIndex; //次の Enum 値に遷移する（行っている）ので、上の public Enum WeightType の中にある Enum の変数、つまり次の変数を、ボタンを押したら表示するよっていうプログラムです。
             value = (value + 1 ) % System.Enum.GetValues(typeof(WeightType)).Length; //Moving to next WeightType, e.g: Like → repost...
             currentWeightIndex = (WeightType)value;
@@ -99,5 +119,6 @@ public class WeightManager : MonoBehaviour
         }
 
         }
+        #endregion
     
 }
