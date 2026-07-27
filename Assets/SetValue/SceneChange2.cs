885ff436c15e76f21129c23a1699b3fbd06dc866
Mata3M2 <kkna49513@gmail.com>
2026-07-27 20:17:42 +1000

    各アームの重み付けを設定できるようにした-Complete base ArMWeightSelect system
    それから重み付けをできるようにしたので、これからはユーザーが ARM の数値を入力できるようにする。Add scene management scripts and new scene for arm selection
    
    - Created ChangeSceneManager.cs to handle scene transitions to "SetArmValue" and "After".
    - Implemented SceneChange2.cs for transitioning to the "ArmSelect" scene.
    - Added corresponding .meta files for both scripts to maintain Unity asset metadata.
    - Introduced a new Unity scene file ArmSelect.unity with its .meta file.

--- Assets/SetValue/SceneChange2.cs
+++ Assets/SetValue/SceneChange2.cs
@@ -1,12 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+
 using UnityEngine.SceneManagement;
 
-public class ChangeScene : MonoBehaviour
+public class SceneChange2 : MonoBehaviour
 {
-    public void change_button()
+    public void change_scene2()
     {
-        SceneManager.LoadScene("SetArmValue");
+        SceneManager.LoadScene("ArmSelect");
     }
 }
--- Assets/SetValue/WeightManager.cs
+++ Assets/SetValue/WeightManager.cs
@@ -1,17 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
+using UnityEditor.SearchService;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class WeightManager : MonoBehaviour
 {
      // Variable to store the weight value   
      public PanelManager panelManager;  
      public WeightSliderController sliderController;
+     public ChangeSceneManager changeSceneManager;
     
 
-    public enum WeightType  {Like = 0, Repost=1, Bookmark=2, Comment=3} //Enumeration type(enum is a special data type that enables for a variable to be a set of predefined constants)
+    public enum WeightType  {Like = 0, Repost=1, Bookmark=2, Comment=3, ToNextPage=4} //Enumeration type(enum is a special data type that enables for a variable to be a set of predefined constants)
 
     [Header("Current Mode）")]
     public WeightType currentWeightIndex; // Current weight type
@@ -40,15 +43,11 @@ public class WeightManager : MonoBehaviour
             sliderController.OnWeightChanged(weight);
         }
     }
-    void Update()
-    {
-         
-    }
     public void OnNextButton()
         {
             float currentSliderValue = sliderController.weight;
             
-            switch (currentWeightIndex)
+            switch (currentWeightIndex) //ここは重み付けをするときにスコアを設定するプログラムです
             {
                 case WeightType.Like: //数値を確定する
                 weightValues[0] = currentSliderValue; 
@@ -60,34 +59,43 @@ public class WeightManager : MonoBehaviour
 
                 case WeightType.Repost:
                 weightValues[1] = currentSliderValue;
-                weight = sliderController.weightSlider.value; //ウェイトスライダーの値をゲットしてる
+                weight = sliderController.weightSlider.value;
                 weight = float.Parse(weight.ToString("F1"));
                 score_Repost.text = "Repost: " + weight;
                 break;
 
                 case WeightType.Bookmark:
-                weight = sliderController.weightSlider.value; //ウェイトスライダーの値をゲットしてる
+                weight = sliderController.weightSlider.value; 
                 weight = float.Parse(weight.ToString("F1"));
                 score_Bookmark.text = "Bookmark: " + weight;
                 break;
 
                 case WeightType.Comment:
                 weightValues[3] = currentSliderValue;
-                weight = sliderController.weightSlider.value; //ウェイトスライダーの値をゲットしてる
+                weight = sliderController.weightSlider.value; 
                 weight = float.Parse(weight.ToString("F1"));
                 score_Comment.text = "Comment: " + weight;
                 break;
-            }
+                
+                
 
-            int value = (int)currentWeightIndex; //Move to next enum
+            }
+            
+
+            
+            int value = (int)currentWeightIndex; //次の Enum 値に遷移する（行っている）ので、上の public Enum WeightType の中にある Enum の変数、つまり次の変数を、ボタンを押したら表示するよっていうプログラムです。
             value = (value + 1 ) % System.Enum.GetValues(typeof(WeightType)).Length; //Moving to next WeightType, e.g: Like → repost...
             currentWeightIndex = (WeightType)value;
 
             Debug.Log(currentWeightIndex);
 
             if (panelManager != null)
-        {
+            {
             panelManager.RefreshPanels();
+            }
+            else if (panelManager == null)
+        {
+            Debug.Log("Panel is dosen't attached");
         }
 
         }
